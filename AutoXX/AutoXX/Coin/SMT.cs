12fb86e6e5322428c7717c3632ffa97c460fca89
enter <enter@DESKTOP-O16R8JK>
2018-02-10 17:19:31 +0800

    1

--- AutoXX/AutoXX/Coin/SMT.cs
+++ AutoXX/AutoXX/Coin/SMT.cs
@@ -6,56 +6,15 @@ using System.Threading.Tasks;
 
 namespace AutoXX.Coin
 {
-    public class SMT
+    public class SMT : BaseCoin
     {
+        public static string coin = "smt";
+        public static decimal buyAmount = (decimal)10;
+        public static decimal sellAmount = (decimal)9.8;
+
         public static void Do()
         {
-            // 获取最近行情
-            string accountId = "1040955";
-            string coin = "smt";
-            decimal lastLow;
-            decimal nowOpen;
-            var flexPointList = new CoinAnalyze().Analyze(coin, "usdt", out lastLow, out nowOpen);
-            // 分析是否下跌， 下跌超过一定数据，可以考虑
-            Console.WriteLine(flexPointList[0].isHigh);
-            var list = new CoinDao().ListNoSellRecord(coin);
-            if (!flexPointList[0].isHigh)
-            {
-                // 最后一次是高位
-                if (nowOpen > flexPointList[0].open * (decimal)1.005 && nowOpen < flexPointList[0].open * (decimal)1.01)
-                {
-                    var b = new AccountOrder().AccountBalance(accountId);
-
-                    if (list.Count <= 0)
-                    {
-                        // 可以考虑
-                        ResponseOrder order = new AccountOrder().NewOrderBuy(accountId, 10, decimal.Round(nowOpen * (decimal)1.005, 4), null, coin, "usdt");
-                        new CoinDao().InsertLog(new BuyRecord()
-                        {
-                            BuyCoin = coin,
-                            BuyPrice = nowOpen * (decimal)1.005,
-                            BuyDate = DateTime.Now,
-                            HasSell = false,
-                        });
-                    }
-                }
-            }
-            // 查询数据库中已经下单数据，如果有，则比较之后的最高值，如果有，则出售
-            if (list.Count > 0)
-            {
-                foreach (var item in list)
-                {
-                    // 分析是否 大于
-                    decimal itemNowOpen = 0;
-                    decimal higher = new CoinAnalyze().AnalyzeNeedSell(item.BuyPrice, item.BuyDate, coin, "usdt", out itemNowOpen);
-                    if (item.BuyPrice * (decimal)1.05 < higher && itemNowOpen * (decimal)1.005 < higher)
-                    {
-                        // 出售
-                        ResponseOrder order = new AccountOrder().NewOrderSell(accountId, (decimal)9.8, decimal.Round(itemNowOpen * (decimal)0.98, 4), null, coin, "usdt");
-                        new CoinDao().SetHasSell(item.Id);
-                    }
-                }
-            }
+            BaseRun(coin, buyAmount, sellAmount);
         }
     }
 }
--- AutoXX/AutoXX/Coin/XEM.cs
+++ AutoXX/AutoXX/Coin/XEM.cs
@@ -8,52 +8,13 @@ namespace AutoXX.Coin
 {
     public class XEM : BaseCoin
     {
+        public static string coin = "xem";
+        public static decimal buyAmount = (decimal)10;
+        public static decimal sellAmount = (decimal)9.8;
+
         public static void Do()
         {
-            // 获取最近行情
-            decimal lastLow;
-            decimal nowOpen;
-            var flexPointList = new CoinAnalyze().Analyze("xem", "usdt", out lastLow, out nowOpen);
-            // 分析是否下跌， 下跌超过一定数据，可以考虑
-            Console.WriteLine(flexPointList[0].isHigh);
-            var list = new CoinDao().ListNoSellRecord("xem");
-            if (!flexPointList[0].isHigh)
-            {
-                // 最后一次是高位
-                // nowOpen > flexPointList[0].open * (decimal)1.005 && nowOpen < flexPointList[0].open * (decimal)1.01
-                if (list.Count <= 0 && CheckCanBuy(nowOpen, flexPointList[0].open))
-                {
-                    var b = new AccountOrder().AccountBalance(accountId);
-                   
-                    // 可以考虑
-                    ResponseOrder order = new AccountOrder().NewOrderBuy(accountId, 10, decimal.Round(nowOpen * (decimal)1.005, 4), null, "xem", "usdt");
-                    new CoinDao().InsertLog(new BuyRecord()
-                    {
-                        BuyCoin = "xem",
-                        BuyPrice = nowOpen * (decimal)1.005,
-                        BuyDate = DateTime.Now,
-                        HasSell = false,
-                    });
-                   
-                }
-            }
-            // 查询数据库中已经下单数据，如果有，则比较之后的最高值，如果有，则出售
-            if (list.Count > 0)
-            {
-                foreach (var item in list)
-                {
-                    // 分析是否 大于
-                    decimal itemNowOpen = 0;
-                    decimal higher = new CoinAnalyze().AnalyzeNeedSell(item.BuyPrice, item.BuyDate, "xem", "usdt", out itemNowOpen);
-                   
-                    if(CheckCanSell(item.BuyPrice, higher, itemNowOpen))
-                    {
-                        // 出售
-                        ResponseOrder order = new AccountOrder().NewOrderSell(accountId, (decimal)9.8, decimal.Round(itemNowOpen * (decimal)0.98, 4), null, "xem", "usdt");
-                        new CoinDao().SetHasSell(item.Id);
-                    }
-                }
-            }
+            BaseRun(coin, buyAmount, sellAmount);
         }
     }
 }
