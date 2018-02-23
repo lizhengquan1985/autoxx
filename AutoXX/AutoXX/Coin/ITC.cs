2d7155744c768c7f8e24f61d6beb042056ef2eb2
enter <enter@DESKTOP-O16R8JK>
2018-02-23 20:32:19 +0800

    1

---
+++ AutoXX/AutoXX/Coin/ITC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoXX.Coin
+{
+    public class ITC : BaseCoin
+    {
+        public static string coin = "itc";
+        public static decimal buyAmount = (decimal)4;
+        public static decimal sellAmount = (decimal)3.9;
+
+        public static void Do()
+        {
+            BaseRun(coin, buyAmount, sellAmount);
+        }
+    }
+}
